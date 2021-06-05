3180dc3f59dfbd2f699afe84fb8f07257e9036c5
tunji17 <tunjiabioye@outlook.com>
2021-06-05 15:42:58 +0100

    boiler plate browser extension code

---
+++ src/popup/component.tsx
@@ -0,0 +1,19 @@
+import React, { FunctionComponent } from "react";
+import { browser } from "webextension-polyfill-ts";
+import "./styles.scss";
+
+// // // //
+
+export const Popup: FunctionComponent = () => {
+    // Sends the `popupMounted` event
+    React.useEffect(() => {
+        browser.runtime.sendMessage({ popupMounted: true });
+    }, []);
+
+    // Renders the component tree
+    return (
+        <div className="popup-container">
+            <h2>Browser Extension</h2>
+        </div>
+    );
+};
