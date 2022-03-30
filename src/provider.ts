66f572f72ec7516a80193b26c98fa6c369762a51
Andres Eduardo Rivero Salguedo <aeriverosalguedo@gmail.com>
2022-03-30 12:26:32 -0400

    add RPC call

--- src/provider.ts
+++ src/provider.ts
@@ -1,19 +1,17 @@
-import axios from "axios";
+const path = require('path');
+const PROTO_PATH = path.resolve(__dirname, './node.proto');
 
-const URL = 'https://web3.test:9999/';
-// const URL = 'https://product-details.mozilla.org/1.0/firefox_versions.json'; test is connected
+const GRPCClient = require('node-grpc-client');
 
+const url = 'https://web3.test:9999';
+
+const myClient = new GRPCClient(PROTO_PATH, 'packageservice', 'Services', url);
 interface RequestArguments {
     readonly method: string;
     readonly params?: readonly unknown[] | object;
 }
 
-interface ProviderRpcError extends Error {
-    code: number;
-    data?: unknown;
-}
-
-interface ProviderMessage {
+interface ExpectedResponse {
     readonly type: string;
     readonly data: unknown;
 }
@@ -24,21 +22,20 @@ interface ProviderInterface {
 }
 
 
+
+// options is optional and is supported from version 1.5.0
+const options = {
+    metadata: {
+        info: 'test'
+    }
+};
+
+
 const provider: ProviderInterface = {
     request: async function (request: RequestArguments) {
         try {
-            const response = await axios.post(URL, request);
-            if (response.data){
-                console.log(response.data);
-                const message: ProviderMessage = response.data;
-                return message
-            }
-
-            if (response.status !== 200){
-                console.log(response);
-                const messageError: ProviderRpcError = response.data;
-                return messageError
-            }
+            const response: ExpectedResponse = await myClient.requestSync(request, options);
+            console.log('The answer of request 1: ', response);
                 
         } catch (error) {
             console.error(error);
@@ -47,15 +44,12 @@ const provider: ProviderInterface = {
     },
     isConnected: async function () {
         try {
-            const response = await axios.get(URL);
-            console.log(response)
-            if (response.status = 200) {
-                return true
-            }
-            return false
+            const response: ExpectedResponse = await myClient.requestSync();
+            console.log('The answer of request 1: ', response);
+                
         } catch (error) {
-            console.log(error)
-            return false
+            console.error(error);
+            return error
         }
     }
 };
