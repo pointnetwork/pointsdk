8fc359412c6e1946146e9046d13c4c42d7103e99
JatinBumbra <jatin.bumbra.23@gmail.com>
2022-04-20 14:11:00 +0530

    Revert back to MUI for the Confirmation Window UI to be consistent with dashboard branding

--- src/confirmation-window/ConfirmationWindow.tsx
+++ src/confirmation-window/ConfirmationWindow.tsx
@@ -1,5 +1,26 @@
 import browser from "webextension-polyfill";
-import React, { ReactEventHandler, Fragment } from "react";
+import React, { ReactEventHandler } from "react";
+// Components
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+// Theme
+import ThemeProvider from "@mui/material/styles/ThemeProvider";
+import createTheme from "@mui/material/styles/createTheme";
+import deepPurple from "@mui/material/colors/deepPurple";
+import blueGrey from "@mui/material/colors/blueGrey";
+
+const theme = createTheme({
+    typography: {
+        fontFamily: "Arial",
+    },
+    palette: {
+        primary: {
+            main: deepPurple.A200,
+            light: blueGrey[50],
+        },
+    },
+});
 
 const data = {
     From: "0x916f8e7566dd63d7c444468cadea37e80f7f8048 (Your account)",
@@ -20,29 +41,60 @@ const ConfirmationWindow = () => {
     };
 
     return (
-        <div className="confirmation-container">
-            <h5 className="confirmation-title">{window.location.origin}</h5>
-            <p>is trying to send a transaction</p>
-            <div className="confirmation-data">
-                {Object.entries(data).map(([key, value], index) => (
-                    <Fragment key={index}>
-                        <b>{key}</b>
-                        <p>{value}</p>
-                    </Fragment>
-                ))}
-            </div>
-            <div className="confirmation-buttons">
-                <button className="button button-primary" onClick={handleAllow}>
-                    Allow
-                </button>
-                <button
-                    className="button button-secondary"
-                    onClick={handleCancel}
+        <ThemeProvider theme={theme}>
+            <Box px="1rem" py="1.25rem" bgcolor="white">
+                <Typography
+                    sx={{
+                        overflowWrap: "break-word",
+                        wordWrap: "break-word",
+                    }}
                 >
-                    Cancel
-                </button>
-            </div>
-        </div>
+                    <Typography variant="h5" fontWeight="bold">
+                        {window.location.origin}
+                    </Typography>
+                    is trying to send a transaction
+                </Typography>
+                <Box
+                    p="0.8rem"
+                    my="1rem"
+                    bgcolor={blueGrey[50]}
+                    borderRadius={2}
+                >
+                    {Object.entries(data).map(([key, value], index) => (
+                        <Box
+                            key={index}
+                            my={
+                                !index ||
+                                index === Object.entries(data).length - 1
+                                    ? 0
+                                    : 1
+                            }
+                        >
+                            <Typography variant="body2" fontWeight="600">
+                                {key}
+                            </Typography>
+                            <Typography
+                                variant="body2"
+                                sx={{
+                                    overflowWrap: "break-word",
+                                    wordWrap: "break-word",
+                                }}
+                            >
+                                {value}
+                            </Typography>
+                        </Box>
+                    ))}
+                </Box>
+                <Box display="flex" justifyContent="flex-end" gap={1} mb={3}>
+                    <Button variant="contained" onClick={handleCancel}>
+                        Allow
+                    </Button>
+                    <Button variant="outlined" onClick={handleAllow}>
+                        Cancel
+                    </Button>
+                </Box>
+            </Box>
+        </ThemeProvider>
     );
 };
 
