01b4e460a90ad9c252f5880e0412c55562a4675c
Koochr <koochr@protonmail.com>
2022-04-27 20:11:12 +0100

    Implement chain switching

--- src/popup/component.tsx
+++ src/popup/component.tsx
@@ -1,23 +1,15 @@
 import React from "react";
-import { browser } from "webextension-polyfill-ts";
 import { Toolbar } from "pointsdk/popup/components/toolbar";
 import { Navigation } from "pointsdk/popup/components/navigation";
-import { Apps } from "pointsdk/popup/components/apps";
 import { AppProvider } from "pointsdk/popup/context";
-import "./style.scss";
+import { NetworkSwitcher } from "pointsdk/popup/components/networkSwitcher";
 
-export const Popup: React.FC = () => {
-    // Sends the `popupMounted` event
-    React.useEffect(() => {
-        browser.runtime.sendMessage({ popupMounted: true });
-    }, []);
-    return (
-        <AppProvider>
-            <div className="popup_container">
-                <Navigation />
-                <Toolbar />
-                <Apps />
-            </div>
-        </AppProvider>
-    );
-};
+export const Popup: React.FC = () => (
+    <AppProvider>
+        <div className="popup_container">
+            <Navigation />
+            <Toolbar />
+            <NetworkSwitcher />
+        </div>
+    </AppProvider>
+);
--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { Popup } from "./component";
 import "pointsdk/reset.scss";
+import "./style.scss";
 
 browser.tabs.query({ active: true, currentWindow: true }).then(() => {
     ReactDOM.render(<Popup />, document.getElementById("popup"));
