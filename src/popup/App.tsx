7b6008e4c25bca6eef185ce7b3c937cbefde2a5e
JatinBumbra <jatin.bumbra.23@gmail.com>
2022-04-29 16:46:58 +0530

    Show the details of the current user in the UI using the SDK

--- src/popup/App.tsx
+++ src/popup/App.tsx
@@ -1,7 +1,9 @@
-import React, { useEffect, useState } from "react";
+import browser from "webextension-polyfill";
+import React, { Fragment, useEffect, useState } from "react";
 // MUI components
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
+import CircularProgress from "@mui/material/CircularProgress";
 import Divider from "@mui/material/Divider";
 import MenuItem from "@mui/material/MenuItem";
 import Select from "@mui/material/Select";
@@ -19,9 +21,17 @@ const App: React.FC = () => {
     const [globalChainId, setGlobalChainId] = useState<string>("");
     const [host, setHost] = useState<string | null>(null);
     const [hostChainId, setHostChainId] = useState<string>("_unset");
+    const [userData, setUserData] = useState<{
+        address: string;
+        balance: number;
+        identity: string;
+    }>({
+        address: "",
+        balance: 0,
+        identity: "",
+    });
 
     const getCurrentChainIds = async () => {
-        setLoading(true);
         try {
             const [globalChainIdRes, tabs] = await Promise.all([
                 browser.storage.local.get("chainIdGlobal"),
@@ -43,11 +53,26 @@ const App: React.FC = () => {
         } catch (e) {
             console.error("Failed to get chain ids: ", e);
         }
-        setLoading(false);
     };
 
     useEffect(() => {
-        void getCurrentChainIds();
+        const init = async () => {
+            setLoading(true);
+            await getCurrentChainIds();
+            let res = await window.point.wallet.address();
+            const address = res.data.address;
+            res = await window.point.identity.ownerToIdentity({
+                owner: address,
+            });
+            const identity = res.data.identity;
+            res = await window.point.wallet.balance();
+            const balance = Number(
+                Number(res.data.balance) / 1000000000000000000,
+            ).toFixed(8);
+            setUserData({ address, balance, identity });
+            setLoading(false);
+        };
+        void init();
     }, []);
 
     const handleGlobalNetworkChange = async (e: SelectChangeEvent) => {
@@ -81,98 +106,130 @@ const App: React.FC = () => {
                     PointSDK
                 </Typography>
             </Box>
-            <Box p={1}>
-                <Typography variant="caption" sx={{ opacity: 0.7 }}>
-                    Global Chain ID:
-                </Typography>
-                <Select
-                    sx={{ my: 1 }}
-                    size="small"
-                    fullWidth
-                    value={globalChainId}
-                    onChange={handleGlobalNetworkChange}
-                    disabled={!globalChainId || loading}
-                >
-                    <MenuItem value="ynet">Ynet</MenuItem>
-                    <MenuItem value="rinkeby">Rinkeby</MenuItem>
-                </Select>
-                {host && (
-                    <>
-                        <Typography fontWeight="bold" marginTop="20px">
-                            {host} Chain ID:
+            {loading ? (
+                <Box display="flex" px={12} py={20}>
+                    <CircularProgress size={24} />
+                    <Typography ml={1}>Loading...</Typography>
+                </Box>
+            ) : (
+                <Fragment>
+                    <Box p={1}>
+                        <Typography variant="caption" sx={{ opacity: 0.7 }}>
+                            Global Chain ID:
                         </Typography>
                         <Select
+                            sx={{ mt: "2px", mb: 2 }}
+                            size="small"
                             fullWidth
-                            value={hostChainId}
-                            onChange={handleHostNetworkChange}
-                            disabled={!host || loading}
+                            value={globalChainId}
+                            onChange={handleGlobalNetworkChange}
+                            disabled={!globalChainId || loading}
                         >
-                            <MenuItem value="_unset">Not Set</MenuItem>
                             <MenuItem value="ynet">Ynet</MenuItem>
                             <MenuItem value="rinkeby">Rinkeby</MenuItem>
                         </Select>
-                    </>
-                )}
-            </Box>
-            <Divider />
-            <Box p={1}>
-                <Typography variant="caption" sx={{ opacity: 0.7 }}>
-                    Logged in as
-                </Typography>
-                <Box display="flex" py={1} alignItems="center">
-                    <AccountCircleIcon fontSize="large" />
-                    <Box mx={1}>
-                        <Typography fontWeight="bold">@jatinbumbra</Typography>
-                        <Typography fontFamily="monospace" variant="caption">
-                            0xeA6671e4E16B35DEe22ec42b155CD00b468cBef0
-                        </Typography>
+                        {host && (
+                            <>
+                                <Box display="flex">
+                                    <Typography
+                                        variant="caption"
+                                        sx={{ opacity: 0.7 }}
+                                    >
+                                        Chain ID for:
+                                    </Typography>
+                                    <Typography
+                                        variant="body2"
+                                        fontWeight="bold"
+                                        ml="3px"
+                                    >
+                                        https://{host}
+                                    </Typography>
+                                </Box>
+                                <Select
+                                    sx={{ mt: "2px", mb: 2 }}
+                                    size="small"
+                                    fullWidth
+                                    value={hostChainId}
+                                    onChange={handleHostNetworkChange}
+                                    disabled={!host || loading}
+                                >
+                                    <MenuItem value="_unset">Not Set</MenuItem>
+                                    <MenuItem value="ynet">Ynet</MenuItem>
+                                    <MenuItem value="rinkeby">Rinkeby</MenuItem>
+                                </Select>
+                            </>
+                        )}
                     </Box>
-                </Box>
-            </Box>
-            <Divider />
-            <Box p={1} mb={1}>
-                <Typography variant="caption" sx={{ opacity: 0.7 }}>
-                    Available Balance
-                </Typography>
-                <Box display="flex" alignItems="baseline" my={1}>
-                    <Typography variant="h4" mr={1}>
-                        0.988543333
-                    </Typography>
-                    <Typography>POINT</Typography>
-                </Box>
-                <Button size="small" variant="contained" endIcon={<SendIcon />}>
-                    Send Money
-                </Button>
-            </Box>
-            <Divider />
-            <Box p={1}>
-                <Typography variant="caption" sx={{ opacity: 0.7 }}>
-                    Quick Links
-                </Typography>
-                <Box display="flex" alignItems="baseline" my={1}>
-                    <Box
-                        display="flex"
-                        alignItems="center"
-                        mr={2}
-                        sx={{ cursor: "pointer" }}
-                    >
-                        <HomeIcon fontSize="small" />
-                        <Typography variant="body2" ml="2px">
-                            Point Explorer
+                    <Divider />
+                    <Box p={1}>
+                        <Typography variant="caption" sx={{ opacity: 0.7 }}>
+                            Logged in as
                         </Typography>
+                        <Box display="flex" py={1} alignItems="center">
+                            <AccountCircleIcon fontSize="large" />
+                            <Box mx={1}>
+                                <Typography fontWeight="bold">
+                                    @{userData.identity}
+                                </Typography>
+                                <Typography
+                                    fontFamily="monospace"
+                                    variant="caption"
+                                >
+                                    {userData.address}
+                                </Typography>
+                            </Box>
+                        </Box>
                     </Box>
-                    <Box
-                        display="flex"
-                        alignItems="center"
-                        sx={{ cursor: "pointer" }}
-                    >
-                        <AccountBalanceWalletIcon fontSize="small" />
-                        <Typography variant="body2" ml="2px">
-                            My Wallet
+                    <Divider />
+                    <Box p={1} mb={1}>
+                        <Typography variant="caption" sx={{ opacity: 0.7 }}>
+                            Available Balance
                         </Typography>
+                        <Box display="flex" alignItems="baseline" my={1}>
+                            <Typography variant="h4" mr={1}>
+                                {userData.balance}
+                            </Typography>
+                            <Typography>POINT</Typography>
+                        </Box>
+                        <Button
+                            size="small"
+                            variant="contained"
+                            endIcon={<SendIcon />}
+                        >
+                            Send Money
+                        </Button>
                     </Box>
-                </Box>
-            </Box>
+                    <Divider />
+                    <Box p={1}>
+                        <Typography variant="caption" sx={{ opacity: 0.7 }}>
+                            Quick Links
+                        </Typography>
+                        <Box display="flex" alignItems="baseline" my={1}>
+                            <Box
+                                display="flex"
+                                alignItems="center"
+                                mr={2}
+                                sx={{ cursor: "pointer" }}
+                            >
+                                <HomeIcon fontSize="small" />
+                                <Typography variant="body2" ml="2px">
+                                    Point Explorer
+                                </Typography>
+                            </Box>
+                            <Box
+                                display="flex"
+                                alignItems="center"
+                                sx={{ cursor: "pointer" }}
+                            >
+                                <AccountBalanceWalletIcon fontSize="small" />
+                                <Typography variant="body2" ml="2px">
+                                    My Wallet
+                                </Typography>
+                            </Box>
+                        </Box>
+                    </Box>
+                </Fragment>
+            )}
         </UIThemeProvider>
     );
 };
--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -1,7 +1,12 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import App from "./App";
+// PointSDK
+import getSdk from "pointsdk/pointsdk/sdk";
+const version = browser.runtime.getManifest().version;
+const point = getSdk("https://point", version);
 
 void browser.tabs.query({ active: true, currentWindow: true }).then(() => {
+    window.point = point;
     ReactDOM.render(<App />, document.getElementById("popup"));
 });
