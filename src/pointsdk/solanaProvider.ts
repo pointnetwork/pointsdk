f0dd901032b63d3cceee1c0d2dab1f8e7d7ad624
Koochr <koochr@protonmail.com>
2022-06-04 00:58:59 +0100

    Implement wallet send method

--- src/pointsdk/solanaProvider.ts
+++ src/pointsdk/solanaProvider.ts
@@ -20,7 +20,7 @@ const getSolanaProvider = () => {
                             message: e.data.error.message,
                         });
                     } else {
-                        resolve(e.data.result);
+                        resolve(e.data);
                     }
                 }
             };
