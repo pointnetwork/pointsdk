40a14e80d9a061a5ceba414fed031acb9d690268
Koochr <koochr@protonmail.com>
2022-06-08 00:02:52 +0100

    Add basic API for getting ERC20 tokens

---
+++ src/constants/tokens.ts
@@ -0,0 +1,19 @@
+const TOKENS = {
+    ynet: [],
+    rinkeby: [
+        {
+            name: "USDC",
+            address: "0x87284d4150b0FADe12255A7d208AD46526C519ee",
+        },
+        {
+            name: "USDT",
+            address: "0xd92e713d051c37ebb2561803a3b5fbabc4962431",
+        },
+        {
+            name: "DAI",
+            address: "0x95b58a6Bff3D14B7DB2f5cb5F0Ad413DC2940658",
+        },
+    ],
+} as const;
+
+export default TOKENS;
--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -3,5 +3,7 @@ export {};
 declare global {
     interface Window {
         point: any;
+        ethereum: any;
+        solana: any;
     }
 }
