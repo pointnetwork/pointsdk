b3341e433844b79afa7eeeb9e7b59cee5f613df7
Koochr <koochr@protonmail.com>
2022-06-20 01:30:42 +0100

    Implement confirmation for wallet send

--- src/pointsdk/pageMessaging.ts
+++ src/pointsdk/pageMessaging.ts
@@ -1,7 +1,7 @@
 import browser from "webextension-polyfill";
 
 window.addEventListener("message", async (e) => {
-    if (e.data.__message_type === "rpc") {
+    if (["rpc", "registerHandler"].includes(e.data.__message_type)) {
         const { __direction, __page_req_id, ...payload } = e.data;
         try {
             const res = await browser.runtime.sendMessage({
