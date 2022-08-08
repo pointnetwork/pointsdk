3ae5a692332779eeb86833b0ed3857663a42400f
German Viescas <german@pointlabs.org>
2022-08-08 12:53:28 -0300

    Do not duplicate `theme` and pick colors from it

--- src/confirmation-window/App.tsx
+++ src/confirmation-window/App.tsx
@@ -1,10 +1,13 @@
 import React, { FunctionComponent } from "react";
 import { BrowserRouter } from "react-router-dom";
+import UIThemeProvider from "pointsdk/theme/UIThemeProvider";
 import ConfirmationWindow from "./ConfirmationWindow";
 
 const App: FunctionComponent = () => (
     <BrowserRouter>
-        <ConfirmationWindow />
+        <UIThemeProvider>
+            <ConfirmationWindow />
+        </UIThemeProvider>
     </BrowserRouter>
 );
 
